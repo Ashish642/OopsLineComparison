375e1dfd0ba7173161dfa21fdeb9ceba9ee6a10e
Ashish <ashishkumarvish.432@gmail.com>
2021-10-13 00:03:42 +0530

    Lenth of line

--- src/com/blz/linecom/LineComparision.java
+++ src/com/blz/linecom/LineComparision.java
@@ -3,5 +3,11 @@ package com.blz.linecom;
 public class LineComparision {
     public static void main(String[] args) {
         System.out.println("Welcome to line compution program");
+        int x1 = 5;
+        int x2 = 8;
+        int y1 = 9;
+        int y2 = 9;
+        double lengthOfLine1 = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+        System.out.println("Length of the Line : "+lengthOfLine1);
     }
 }
