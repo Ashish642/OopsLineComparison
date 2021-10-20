1621eb5a027cacf0f80cea9caa8392821b0b0d4f
Ashish <ashishkumarvish.432@gmail.com>
2021-10-20 11:21:24 +0530

    Using Oops Method

--- src/com/blz/linecom/LineComparision.java
+++ src/com/blz/linecom/LineComparision.java
@@ -1,30 +1,68 @@
 package com.blz.linecom;
-
+import java.util.Scanner;
 public class LineComparision {
     public static void main(String[] args) {
         System.out.println("Welcome to line compution program");
-        int x1 = 5;
-        int x2 = 8;
-        int y1 = 9;
-        int y2 = 89;
-        int p1 = 14;
-        int p2 = 16;
-        int q1 = 4;
-        int q2 = 85;
-        double lengthOfLine1 = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
-        double lengthOfLine2 = Math.sqrt((p2-p1) * (p2-p1) + (q2-q1) * (q2-q1));
-        System.out.println("Length of the First Line : "+lengthOfLine1);
-        System.out.println("Length of the Second Line : "+lengthOfLine2);
-
-        if (lengthOfLine1 > lengthOfLine2) {
-            System.out.println("Line1 greater than Line2");
-
-        }
-        else if(lengthOfLine1 < lengthOfLine2) {
-            System.out.println("Line1 is lessar than Line2");
-        }
-        else {
-            System.out.println("Both line are equal");
-        }
+        // create a scanner object class
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the co-ordinate for x-axis of first line : ");
+        int x1 = sc.nextInt();
+        int x2 = sc.nextByte();
+        System.out.println("Enter the co-ordinate for y-axis of first line : ");
+        int y1 = sc.nextInt();
+        int y2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate for x-axis of second line : ");
+        int p1 = sc.nextInt();
+        int p2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate for y-axis of second line : ");
+        int q1 = sc.nextInt();
+        int q2 = sc.nextInt();
+        // creating object
+        LineComparision length = new LineComparision();
+        double lengthOfLine1 = length.lengthFind(x1, x2, y1, y2);
+        System.out.println("Length of the first line is : " + lengthOfLine1);
+        double lengthOfLine2 = length.lengthFind(p1, p2, q1, q2);
+        System.out.println("Length of the Second Line  is: " + lengthOfLine2);
+        // calling method
+        length.checkEquality(lengthOfLine1, lengthOfLine2);
+        length.checkCompareTo(lengthOfLine1, lengthOfLine2);
     }
+    public double lengthFind(int x1, int x2,int y1, int y2) {
+        double lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        return lengthOfLine;
+    }
+    /* Method use : -
+    To check equality of line
+     */
+     public static void checkEquality(double lengthOfLine1, double lengthOfLine2) {
+
+         if (lengthOfLine1==lengthOfLine2) {
+             System.out.println("Both line are equal.");
+
+         }
+         else {
+             System.out.println("both lines are not equal.");
+
+         }
+         /* Methode use : -
+         to check compare two line
+          */
+      public static void checkCompareTo(double lengthOfLine1,double lengthOfLine2) {
+          if (lengthOfLine1>lengthOfLine2) {
+              System.out.println("Length of line is greater than Length of Line2");
+
+          }
+          else if(lengthOfLine1<lengthOfLine2) {
+              System.out.println("Length of Line lesser than Length of Line2");
+
+
+          }
+          else {
+              System.out.println("Both line are Equal");
+          }
+         }
+
+
+
+         }
 }
